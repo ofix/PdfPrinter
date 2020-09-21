d9e41b612c9d41333de2f3ada27bd9e2085e54d5
ofix <shb8845369@gmail.com>
2020-09-22 01:10:56 +0800

    修改打印调用接口

--- The Boost C++ Libraries.js
+++ The Boost C++ Libraries.js
@@ -1,3 +1,4 @@
 const PdfPrinter=require('./PdfPrinter');
-let printer = new PdfPrinter('https://theboostcpplibraries.com/','The Boost C++ Libraries');
+let printer = new PdfPrinter('https://httpd.apache.org/docs/trunk/zh-cn/','The Boost C++ Libraries');
+printer.setEntryCss('.toc > li ','span > a','ul > li');
 printer.run();
