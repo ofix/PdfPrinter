6a395434d2515541e56b70c2dc10e399833c9c7a
ofix <shb8845369@gmail.com>
2020-09-29 23:24:25 +0800

    优化FlatPdfPrinter.js

--- progit.js
+++ progit.js
@@ -1,4 +1,4 @@
-const PdfPrinter=require('./GooglePdfPrinter');
+const PdfPrinter=require('./FlatPdfPrinter');
 // var globalTunnel = require('global-tunnel-ng');
  
 // globalTunnel.initialize({
@@ -8,10 +8,16 @@ const PdfPrinter=require('./GooglePdfPrinter');
 //   sockets: 50 // optional pool size for each http and https
 // });
 
-
-let printer = new PdfPrinter('https://gitee.com/progit/','proGit');
-printer.setMenuContainer('.book_toc');
-printer.setUrlPrefix('https://gitee.com/progit/');
+let options={
+    pdfEntry:'https://gitee.com/progit/', //PDF文件访问URL入口
+    pdfName:'proGit', //需要打印的PDF文件名
+    urlPrefix:'https://gitee.com/progit/',//URL前缀，
+    removePrintCss:true,//时候移除打印CSS
+    printContainer:'body',//打印的区域包裹的容器
+    menuContainer:'.book_toc',//打印的菜单容器
+    dirtyInnerElements:['#book-chapters','.bottom-nav'],//容器里面的元素
+}
+let printer = new PdfPrinter(options);
 printer.run();
 
 // globalTunnel.end();
